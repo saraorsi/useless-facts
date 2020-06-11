35b9df0d7c4d4f6f8c958d5341465ac3adf40c3c
Sara Orsi <sararaorsi@gmail.com>
2020-06-11 18:21:42 +0100

    Updates

---
+++ precache-manifest.1a5e3ac7c83b2537bd1707966f848049.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b68496bce7d1af5ed972dbeccf3abb9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bfa3bf1baf0f543255a",
+    "url": "/static/css/main.ab03f65f.chunk.css"
+  },
+  {
+    "revision": "6fae707faeaa9c57f418",
+    "url": "/static/js/2.39a3fe45.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.39a3fe45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bfa3bf1baf0f543255a",
+    "url": "/static/js/main.9ae27732.chunk.js"
+  },
+  {
+    "revision": "ea72eb6aa2937f9dfc5b",
+    "url": "/static/js/runtime-main.571d46f4.js"
+  }
+]);
--- src/components/Header/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Header = () => {
-    return(
-        <header>Useless Facts</header>
-    )
-}
-
-export default Header;
